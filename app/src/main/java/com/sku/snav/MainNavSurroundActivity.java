e109b91a015ba59eccab2d880c07823691079f07
dlwhsk0 <hana8145@gmail.com>
2023-05-21 13:06:23 +0900

    2023-05-21 13:06

--- app/src/main/java/com/sku/snav/MainNavSurroundActivity.java
+++ app/src/main/java/com/sku/snav/MainNavSurroundActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_main_nav extends AppCompatActivity {
+public class MainNavSurroundActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_nav);
+        setContentView(R.layout.activity_main_nav_surround);
     }
 }
