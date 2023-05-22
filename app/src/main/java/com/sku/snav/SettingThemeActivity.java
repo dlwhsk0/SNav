66b133ff3d6627cd59cd316948722647561f769a
dlwhsk0 <hana8145@gmail.com>
2023-05-23 04:45:21 +0900

    2023-05-23 04:45

---
+++ app/src/main/java/com/sku/snav/SettingThemeActivity.java
@@ -0,0 +1,15 @@
+package com.sku.snav;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_theme);
+}
