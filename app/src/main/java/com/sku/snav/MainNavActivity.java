5cb180106361c30d9ab3a7515cf020f939e88e2e
dlwhsk0 <hana8145@gmail.com>
2023-05-22 16:36:36 +0900

    2023-05-22 16:13

--- app/src/main/java/com/sku/snav/MainNavActivity.java
+++ app/src/main/java/com/sku/snav/MainNavActivity.java
@@ -1,14 +1,28 @@
 package com.sku.snav;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainNavActivity extends AppCompatActivity {
+
+    Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_nav);
+
+        startBtn = (Button) findViewById(R.id.startbtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainNavMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
