6b7ccf1442310526b974b92ea2cd7b14ed815d82
dlwhsk0 <hana8145@gmail.com>
2023-05-23 15:56:55 +0900

    2023-05-23 15:56

--- app/src/main/java/com/sku/snav/MainActivity.java
+++ app/src/main/java/com/sku/snav/MainActivity.java
@@ -1,50 +1,19 @@
 package com.sku.snav;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button loginCheck, settingCheck, introCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
-        startActivity(intent);
-
-        loginCheck = (Button) findViewById(R.id.logincheck);
-        settingCheck = (Button) findViewById(R.id.settingcheck);
-        introCheck = (Button) findViewById(R.id.introcheck);
-
-        loginCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        settingCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        introCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
-                startActivity(intent);
-            }
-        });
+        //Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
+        //startActivity(intent);
+        Toast.makeText(getApplicationContext(),"MainActivity가 실행됨",Toast.LENGTH_SHORT).show();
     }
 }
